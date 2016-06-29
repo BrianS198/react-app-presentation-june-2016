bb399b54d7834c5213753cd69a98943d121ed459
Brian Simpson <brian.simpson198@gmail.com>
2016-06-28 19:15:55 -0500

    Add Immutable proptypes, classnames, and fix standards

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const WeaponViewer = require('components/weapon_viewer');
-const MainStyles = require('theme/main.scss');
+const __styles = require('theme/main.scss');
 
-const appContainer = document.getElementById('app-container');
+const appContainer = document.getElementById('app');
 
 ReactDOM.render(<WeaponViewer />, appContainer);
